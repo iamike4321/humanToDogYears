188056237ae01373f1796a45ee9648a5f48fcb01
Isaac Meder <iamike4321@gmail.com>
2017-07-14 12:54:34 -0700

    /7 and *7 made math a bit better 1.02

--- js/js.js
+++ js/js.js
@@ -1,7 +1,10 @@
 function convert(age) {
-    var x;
-    if (age == "H") {x = document.getElementById("h").value /7;
-        document.getElementById("d").value = x.toFixed(1);}
-    else {x = (document.getElementById("d").value) /7;
-        document.getElementById("h").value = x.toFixed(1);}
+  var x;
+  if (age == "H") {
+    x = document.getElementById("h").value * 7;
+    document.getElementById("d").value = x.toFixed(1);
+  } else {
+    x = (document.getElementById("d").value) / 7;
+    document.getElementById("h").value = x.toFixed(1);
+  }
 }
